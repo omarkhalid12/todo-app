4778b591cbf465ed9910504a93108cddd7e7e43a
omarkhalid12 <ok3817554@gmail.com>
2024-03-20 02:14:24 +0200

    Update aProduct & Add Remove Modal

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,7 +2,10 @@ import Button from "./ui/Button";
 import useAuthenticatedQuery from "../hooks/useAuthenticatedQuery";
 import Modal from "./ui/Modal";
 import Input from "./ui/Input";
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import Textarea from "./ui/Textarea";
+import { ITodo } from "../interfaces";
+import axiosInstance from "../config/axios.config";
 
 const TodoList = () => {
 
@@ -11,10 +14,17 @@ const TodoList = () => {
   const userData = userDataString ? JSON.parse(userDataString) : null;
   
   const [isOpenEditModal, setIsOpenEditModal] = useState(false)
+  const [isUpdating, setIsUpdating] = useState(false)
+  const [isOpenConfirmModal, setIsOpenConfirmModal] = useState(false);
+  const [todoEdit, setTodoEdit] = useState<ITodo>({
+    id: 0,
+    title: "",
+    description: ""
+  })
 
   /** FETCH DATA USING REACT QUERY **/
   const {data, isLoading} = useAuthenticatedQuery({url: "/users/me?populate=todos",
-  queryKey: ["todos"], 
+  queryKey: ["todoList", `${todoEdit.id}`], 
   config: {
     headers: {
       Authorization: `Bearer ${userData.jwt}`
@@ -22,30 +32,94 @@ const TodoList = () => {
   }})
 
   // ** Handlers
-  const onToggleEditModal = ()=> {
-    setIsOpenEditModal(prev => !prev)
+  const onCloseEditModal = ()=> {
+    setTodoEdit({
+      id: 0,
+      title: "",
+      description: ""
+    })
+    setIsOpenEditModal(false)
+  }
+  const onOpenEditModal = (todo: ITodo)=> {
+    setTodoEdit(todo)
+    setIsOpenEditModal(true)
+  }
+
+  const closeConfirmModal = ()=> setIsOpenConfirmModal(false)
+  const openConfirmModal = ()=> setIsOpenConfirmModal(true)
+
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)=> {
+    const {value, name} = event.target
+
+    setTodoEdit({
+      ...todoEdit,
+      [name]: value
+    })
+  }
+
+  const submitHandler = async(event: FormEvent<HTMLFormElement>)=> {
+    event.preventDefault()
+
+    setIsUpdating(true)
+
+    const {title, description} = todoEdit
+    try {
+      const { status } = await axiosInstance.put(`/todos/${todoEdit.id}`, {data: title, description}, 
+      {headers: {
+        Authorization: `Bearer ${userData.jwt}`
+      }})
+      if(status == 200) {
+        onCloseEditModal()
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsUpdating(false)
+    }
   }
 
   if(isLoading) return <h3>Loading ...</h3> 
 
   return (
     <div className="space-y-1 ">
-      {data.todos.length ? data.todos.map(todo => (
+      {data.todos.length ? data.todos.map((todo: ITodo) => (
         <div key={todo.id} className="flex items-center justify-between hover:bg-gray-100 duration-300 p-3 rounded-md even:bg-gray-100">
           <p className="w-full font-semibold">1 - {todo.title}</p>
           <div className="flex items-center justify-end w-full space-x-3">
-            <Button size={"sm"} onClick={onToggleEditModal}>Edit</Button>
-            <Button variant={"danger"} size={"sm"}>
+            <Button size={"sm"} onClick={()=> onOpenEditModal(todo)}>Edit</Button>
+            <Button variant={"danger"} size={"sm"} onClick={openConfirmModal}>
               Remove
             </Button>
           </div>
         </div>
       )) : <h3>No todos Yet!</h3>}
-      <Modal isOpen={ isOpenEditModal } closeModal={ onToggleEditModal } title="Edit this todo" >
-        <Input value="EDIT TODO" />
-        <div className="flex item-center space-x-3 mt-4">
-          <Button className="bg-indigo-700 hover:bg-indigo-800">Update</Button>
-          <Button variant={"cancel"}>Cancel</Button>
+      {/* Edit todo Modal */}
+      <Modal isOpen={ isOpenEditModal } closeModal={ onCloseEditModal } title="Edit this todo" >
+        <form className="space-y-3" onSubmit={submitHandler}>
+          <Input name="title" value={todoEdit.title} onChange={onChangeHandler}/>
+          <Textarea name="description" value={todoEdit.description} onChange={onChangeHandler}/>
+          <div className="flex item-center space-x-3 mt-4">
+            <Button className="bg-indigo-700 hover:bg-indigo-800" isLoading={isUpdating}>Update</Button>
+            <Button variant={"cancel"}>Cancel</Button>
+          </div>
+        </form>
+      </Modal>
+      {/* DELETING PRODUCT CONFIRM MODAL */}
+      <Modal
+        isOpen={isOpenConfirmModal}
+        closeModal={closeConfirmModal}
+        title="Are you sure you want to remove this Product from your Store?"
+        description="Deleting this product will remove it permanently from your inventory. 
+        Any associated data, sales history, and other related information will also be deleted. 
+        Please make sure this is the intended action."
+      >
+        <div className="flex items-center space-x-3">
+          <Button variant={"danger"} onClick={()=> {}}>
+            Yes, remove
+          </Button>
+          <Button variant={"cancel"} onClick={closeConfirmModal}>
+            Cancel
+          </Button>
         </div>
       </Modal>
     </div>
